d2c061f5076af76a2db771fe038ff68106d7a64f
lin.zehong <lin.zehong@ztesoft.com>
2018-11-28 15:39:43 +0800

    initial

---
+++ .umirc.js
@@ -0,0 +1,25 @@
+
+// ref: https://umijs.org/config/
+export default {
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: true,
+      title: 'umi-test',
+      dll: true,
+      routes: {
+        exclude: [],
+      },
+      hardSource: true,
+    }],
+  ],
+  "proxy": {
+    "/api": {
+      "target": "http://jsonplaceholder.typicode.com/",
+      "changeOrigin": true,
+      "pathRewrite": { "^/api": "" }
+    }
+  },
+}
